394fa2ec6fad32c07c112a6683bc838743ca66e4
pailecheng <981005580@qq.com>
2023-04-06 09:42:46 +0800

    1.0.3

--- src/views/chat/layout/sider/index.vue
+++ src/views/chat/layout/sider/index.vue
@@ -80,7 +80,7 @@ watch(
           <List />
         </div>
         <div class="p-4">
-          <NButton block @click="show"><!--隐藏功能 block @click="show = true"-->
+          <NButton block @click="show = true"><!--隐藏功能 block @click="show = true"-->
             待定功能...
           </NButton>
         </div>
