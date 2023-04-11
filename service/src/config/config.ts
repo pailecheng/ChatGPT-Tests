5a72355ed0e4668c4d060d81e59ad74b6be68f3a
pailecheng <981005580@qq.com>
2023-04-11 15:20:13 +0800

    66666

---
+++ service/src/config/config.ts
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 3000
+
+export const DB_HOST = process.env.MYSQLHOST || 'localhost'
+export const DB_USER = process.env.MYSQLUSER || 'root'
+export const DB_PASSWORD = process.env.MYSQLPASSWORD || 'faztpassword'
+export const DB_NAME = process.env.MYSQLDATABASE || 'usersdb'
+export const DB_PORT = Number(process.env.MYSQLPORT) || 3306
