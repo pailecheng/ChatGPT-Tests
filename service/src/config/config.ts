8674c204a7d64cf048cabd8b6e56b078b3f84664
pailecheng <981005580@qq.com>
2023-04-11 15:45:12 +0800

    23223222

--- service/src/config/config.ts
+++ service/src/config/config.ts
@@ -1,7 +1,6 @@
 export const PORT = process.env.PORT || 3000
-
-export const DB_HOST = process.env.MYSQLHOST || 'localhost'
+export const DB_HOST = process.env.MYSQLHOST || 'containers-us-west-102.railway.app'
 export const DB_USER = process.env.MYSQLUSER || 'root'
-export const DB_PASSWORD = process.env.MYSQLPASSWORD || 'faztpassword'
-export const DB_NAME = process.env.MYSQLDATABASE || 'usersdb'
-export const DB_PORT = Number(process.env.MYSQLPORT) || 3306
+export const DB_PASSWORD = process.env.MYSQLPASSWORD || '9HAVsy8uphnRRVfw2gaS'
+export const DB_NAME = process.env.MYSQLDATABASE || 'railway'
+export const DB_PORT = Number(process.env.MYSQLPORT) || 6302
