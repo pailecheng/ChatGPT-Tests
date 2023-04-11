d1a9938230d7505453f8544da7563d4bb708734c
pailecheng <981005580@qq.com>
2023-04-11 09:41:50 +0800

    666

--- service/src/index.ts
+++ service/src/index.ts
@@ -54,7 +54,7 @@ router.post('/config', auth, async (req, res) => {
 })
 
 router.post('/tests', async (req, res) => {
-    res.send(req.body)
+    res.send('OK!')
 })
 
 router.post('/session', async (req, res) => {
