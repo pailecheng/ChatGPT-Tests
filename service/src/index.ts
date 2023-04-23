26879a97539d65b2f348271ea3a4e9bb9e7f738c
pailecheng <981005580@qq.com>
2023-04-23 10:05:04 +0800

    999

--- service/src/index.ts
+++ service/src/index.ts
@@ -5,17 +5,9 @@ import { chatConfig, chatReplyProcess, currentModel } from './chatgpt'
 import { auth } from './middleware/auth'
 import { limiter } from './middleware/limiter'
 import { isNotEmptyString } from './utils/is'
-import mysql, { Pool } from 'mysql2/promise';
+import pool from './db.js'; // 引入数据库连接配置
 const app = express()
 const router = express.Router()
-
-// 从环境变量中获取数据库连接的参数
-const MYSQL_URL = process.env.MYSQL_URL || 'mysql://root:9HAVsy8uphnRRVfw2gaS@containers-us-west-102.railway.app:6302/railway';
-const MYSQL_HOST = process.env.MYSQLHOST || 'containers-us-west-102.railway.app';
-const MYSQL_USER = process.env.MYSQLUSER || 'root';
-const MYSQL_PASSWORD = process.env.MYSQLPASSWORD || '9HAVsy8uphnRRVfw2gaS';
-const MYSQL_DATABASE = process.env.MYSQLDATABASE || 'railway';
-const DB_PORT = Number(process.env.MYSQLPORT) || 6302
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -60,17 +52,6 @@ router.post('/config', auth, async (req, res) => {
     res.send(error)
   }
 })
-const pool: Pool = mysql.createPool({
-  url:MYSQL_URL,
-  host: MYSQL_HOST,
-  user: MYSQL_USER,
-  password: MYSQL_PASSWORD,
-  database: MYSQL_DATABASE,
-  port: DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
 router.post('/tests', async (req, res) => {
   let conn;
   try {
