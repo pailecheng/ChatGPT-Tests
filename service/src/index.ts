cc1c4bc168ba4fcc89a9711c6fa5d57b6b649b81
pailecheng <981005580@qq.com>
2023-04-28 10:03:14 +0800

    1111

--- service/src/index.ts
+++ service/src/index.ts
@@ -68,7 +68,7 @@ router.post('/session', async (req, res) => {
   try {
     const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
     const hasAuth = isNotEmptyString(AUTH_SECRET_KEY)
-    res.send({ status: 'Success', message: AUTH_SECRET_KEY, data: { auth: hasAuth, model: currentModel() } })
+    res.send({ status: 'Success', message: '', data: { auth: hasAuth, model: currentModel() } })
   }
   catch (error) {
     res.send({ status: 'Fail', message: error.message, data: null })
