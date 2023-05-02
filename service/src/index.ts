a34b38a118b11b31026b591066330c24f018420e
pailecheng <981005580@qq.com>
2023-05-02 23:48:20 +0800

    11111

--- service/src/index.ts
+++ service/src/index.ts
@@ -56,8 +56,8 @@ router.post('/tests', async (req, res) => {
   let conn;
   try {
     conn = await pool.getConnection();
-    const [rows, fields] = await conn.query('INSERT INTO UserKeys (id, secretkey) VALUES (?, ?)', ['8', 'bbbbb'])
-    res.send({ message: "查询成功！", data: rows })
+    const data = await conn.query('SELECT * FROM UserKeys WHERE ID = 1')
+    res.send({ message: "查询成功！", data: data })
   } catch (error) {
     res.status(500).send(error.message);
   }finally {
