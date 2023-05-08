8876d5c05ad40438e00a26f073d2bbd7761c1d23
pailecheng <981005580@qq.com>
2023-05-08 09:52:27 +0800

    121222

--- src/components/common/Setting/Configuration.vue
+++ src/components/common/Setting/Configuration.vue
@@ -12,7 +12,7 @@ const options = [
   { label: 'gpt-3.5-turbo', key: 'gpt-3.5', value: 'gpt-3.5-turbo' },
   { label: 'gpt-4', key: 'gpt-4', value: 'gpt-4' },
 ];
-const selected = ref({ label: 'gpt-3.5-turbo', key: 'gpt-3.5', value: 'gpt-3.5-turbo' });
+const selectedOption= null;
 
 const avatar = ref(userInfo.value.avatar ?? '');
 
@@ -24,11 +24,10 @@ const avatar = ref(userInfo.value.avatar ?? '');
       <div class="flex items-center space-x-4">
         <span class="flex-shrink-0 w-[100px]">模型</span>
         <div class="flex flex-wrap items-center gap-4">
-          <NSelect
-            style="width: 140px"
-            v-model="selected"
-            :options="options"            
-          />
+          <label v-for="option in options" :key="option.value">
+            <input type="radio" :value="option.value" v-model="selectedOption">{{ option.label }}
+          </label>
+          <p>Selected option: {{ selectedOption }}</p>
         </div>
       </div>
       <div class="flex items-center space-x-4">
