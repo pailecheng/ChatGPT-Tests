f0036c3f07c963091daa29036921fcf4e639f4cd
pailecheng <981005580@qq.com>
2023-05-08 10:17:52 +0800

    333333

--- src/components/common/Setting/Configuration.vue
+++ src/components/common/Setting/Configuration.vue
@@ -1,41 +1,45 @@
-<script lang="ts" setup>
-import { computed, ref } from 'vue';
-import { NButton, NInput} from 'naive-ui';
-import { useUserStore } from '@/store';
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { FormRules } from 'naive-ui'
 
-const userStore = useUserStore();
-
-
-const userInfo = computed(() => userStore.userInfo);
-
-const options = [
-  { label: 'gpt-3.5-turbo', key: 'gpt-3.5', value: 'gpt-3.5-turbo' },
-  { label: 'gpt-4', key: 'gpt-4', value: 'gpt-4' },
-];
-
-const avatar = ref(userInfo.value.avatar ?? '');
-
+export default defineComponent({
+  setup () {
+    const localeRef = ref('语言1')
+    const rules: FormRules = {
+      input: {
+        required: true,
+        trigger: ['focus', 'input'],
+        renderMessage: () => {
+          return localeRef.value === '语言1'
+            ? '抽离透传归因分析作为抓手为产品赋能'
+            : '方法论是组合拳达到平台化标准'
+        }
+      }
+    }
+    const modelRef = ref({
+      input: ''
+    })
+    return {
+      rules,
+      model: modelRef,
+      locale: localeRef
+    }
+  }
+})
 </script>
 
 <template>
-  <div class="p-4 space-y-5 min-h-[200px]">
-    <div class="space-y-6">
-      <div class="flex items-center space-x-4">
-        <span class="flex-shrink-0 w-[100px]">模型</span>
-        <div class="flex flex-wrap items-center gap-4">
-          <label v-for="option in options" :key="option.value">
-            <input type="radio" :value="option.value" v-model="$data.selectedOption">{{ option.label }}
-          </label>
-          <p>Selected option: {{ selectedOption }}</p>
-        </div>
-      </div>
-      <div class="flex items-center space-x-4">
-        <span class="flex-shrink-0 w-[100px]">秘钥</span>
-        <div class="flex-1">
-          <NInput v-model:value="avatar" placeholder="" />
-        </div>
-        <NButton size="tiny" text type="primary">保存</NButton>
-      </div>
-    </div>
-  </div>
+  <n-space vertical>
+    <n-radio-group v-model:value="locale">
+      <n-space>
+        <n-radio label="语言1" value="语言1" />
+        <n-radio label="语言2" value="语言2" />
+      </n-space>
+    </n-radio-group>
+    <n-form :model="model" :rules="rules">
+      <n-form-item label="输入点什么去掉 error" path="input">
+        <n-input v-model:value="model.input" />
+      </n-form-item>
+    </n-form>
+  </n-space>
 </template>
