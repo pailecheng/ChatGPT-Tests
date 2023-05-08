c6142bce5f4488d82253cd86ceb9eda3ffa3bd30
pailecheng <981005580@qq.com>
2023-05-08 16:00:35 +0800

    9999

--- src/components/common/Setting/Configuration.vue
+++ src/components/common/Setting/Configuration.vue
@@ -1,18 +1,12 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
-import { FormRules, NButton,NRadioGroup,NRadio, NInput, NSelect } from 'naive-ui';
-
-const options = [
-  { label: 'gpt-3.5-turbo', key: 'gpt-3.5', value: 'gpt-3.5-turbo' },
-  { label: 'gpt-4', key: 'gpt-4', value: 'gpt-4' },
-];
-const selected = ref('gpt-3.5-turbo');
+import { FormRules, NButton,NRadioGroup,NRadio, NInput } from 'naive-ui';
 
 const avatar = ref('abcdaesd');
-const locale = ref('语言1')
+const locale = ref('gpt-3.5')
 const rules: FormRules = {
   input: [{ required: true, trigger: ['focus', 'input'], message: () => {
-    return locale.value === '语言1'
+    return locale.value === 'gpt-3.5'
       ? '抽离透传归因分析作为抓手为产品赋能'
       : '方法论是组合拳达到平台化标准';
   } }]
@@ -22,19 +16,6 @@ const model = ref({
 });
 </script>
 <template>
-  <n-space>
-          <NRadioGroup v-model:value="locale">
-            <n-space>
-              <NRadio label="GPT-3.5-turbo" value="gpt-3.5" />
-              <NRadio label="GPT-4" value="gpt-4" />
-            </n-space>
-          </NRadioGroup>
-          <n-form :model="model" :rules="rules">
-            <n-form-item label="输入点什么去掉 error" path="input">
-              <NInput v-model="model.input" />
-            </n-form-item>
-          </n-form>
-        </n-space>
   <div class="p-4 space-y-5 min-h-[200px]">
     <div class="space-y-6">
       <div class="flex items-center space-x-4">
@@ -43,11 +24,19 @@ const model = ref({
       <div class="flex items-center space-x-4">
         <span class="flex-shrink-0 w-[100px]">模型</span>
         <div class="flex flex-wrap items-center gap-4">
-          <NSelect
-            style="width: 140px"
-            v-model="selected"
-            :options="options"            
-          />
+          <n-space>
+            <NRadioGroup v-model:value="locale">
+              <n-space>
+                <NRadio label="GPT-3.5-turbo" value="gpt-3.5" />
+                <NRadio label="GPT-4" value="gpt-4" />
+              </n-space>
+            </NRadioGroup>
+            <n-form :model="model" :rules="rules">
+              <n-form-item label="输入点什么去掉 error" path="input">
+                <NInput v-model="model.input" />
+              </n-form-item>
+            </n-form>
+          </n-space>
 
         </div>
       </div>
