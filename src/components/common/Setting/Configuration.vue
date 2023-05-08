b324235e9dc93f8c01c8ba7525e0e7b21eda88a6
pailecheng <981005580@qq.com>
2023-05-08 11:30:55 +0800

    66666

--- src/components/common/Setting/Configuration.vue
+++ src/components/common/Setting/Configuration.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
-import { NButton, NInput,NSelect } from 'naive-ui';
+import { FormRules,NButton, NInput,NSelect } from 'naive-ui';
 
 const options = [
   { label: 'gpt-3.5-turbo', key: 'gpt-3.5', value: 'gpt-3.5-turbo' },
@@ -9,12 +9,41 @@ const options = [
 const selected = ref({ label: 'gpt-3.5-turbo', key: 'gpt-3.5', value: 'gpt-3.5-turbo' });
 
 const avatar = ref('abcdaesd' ?? '');
-
+const locale = ref('语言1')
+const rules: FormRules = ref(
+  NInput,{
+        required: true,
+        trigger: ['focus', 'input'],
+        renderMessage: () => {
+          return locale.value === '语言1'
+            ? '抽离透传归因分析作为抓手为产品赋能'
+            : '方法论是组合拳达到平台化标准'
+        }
+  }
+)
+const model = ref({
+      input: ''
+})
 </script>
 
 <template>
   <div class="p-4 space-y-5 min-h-[200px]">
     <div class="space-y-6">
+      <div class="flex items-center space-x-4">
+        <n-space vertical>
+          <n-radio-group v-model:value="locale">
+            <n-space>
+              <n-radio label="语言1" value="语言1" />
+              <n-radio label="语言2" value="语言2" />
+            </n-space>
+          </n-radio-group>
+          <n-form :model="model" :rules="rules">
+            <n-form-item label="输入点什么去掉 error" path="input">
+              <NInput v-model:value="model.input" />
+            </n-form-item>
+          </n-form>
+        </n-space>
+      </div>
       <div class="flex items-center space-x-4">
         <span class="flex-shrink-0 w-[100px]">模型</span>
         <div class="flex flex-wrap items-center gap-4">
