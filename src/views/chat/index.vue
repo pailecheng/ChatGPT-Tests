7762a19772f5c0b19d4d84c0f1d33ef5641ab2dc
pailecheng <981005580@qq.com>
2023-05-10 18:13:58 +0800

    008

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -123,7 +123,6 @@ async function onConversation() {
           const lastIndex = responseText.lastIndexOf('\n', responseText.length - 2)
           let chunk = responseText
           if (lastIndex !== -1)
-            count++
             chunk = responseText.substring(lastIndex)
           try {
             const data = JSON.parse(chunk)
@@ -154,7 +153,7 @@ async function onConversation() {
           catch (error) {
           //
           }
-          
+          count++
         },
       })      
     }
