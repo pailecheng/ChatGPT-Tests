c155642a95f5c487782a9d0b38266df224ae9994
pailecheng <981005580@qq.com>
2023-05-11 09:38:11 +0800

    005

--- src/store/modules/auth/index.ts
+++ src/store/modules/auth/index.ts
@@ -33,7 +33,7 @@ export const useAuthStore = defineStore('auth-store', {
         //设置cookie
         let cookie = Cookies.get('cookieName');
         let cook;
-        if(cookie==null){
+        if(!cookie){
           Cookies.set('cookieName', generateUniqueValue(16));
           cook = generateUniqueValue(16);
         }else{
